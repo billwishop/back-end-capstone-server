39245c195cc54e43e057d08423b38b2dfb385485
billwishop <willbishop.hello@gmail.com>
2021-03-11 10:03:43 -0600

    PUT is set up for payments.

--- crosscheckapi/views/payment.py
+++ crosscheckapi/views/payment.py
@@ -47,6 +47,38 @@ class Payments(ViewSet):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def update(self, request, pk=None):
+        """Handle PUT requests for payments
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        # landlord = authenticated user
+        landlord = Landlord.objects.get(user=request.auth.user)
+        tenant = Tenant.objects.get(pk=request.data["tenant"])
+
+        payment = Payment.objects.get(pk=pk)
+        payment.date = request.data["date"]
+        payment.amount = request.data["amount"]
+        payment.ref_num = request.data["ref_num"]
+        payment.tenant = tenant
+
+        # Find the associated lease to assign the property 
+        # rather than having the user select both the 
+        # tenant and property
+        lease = TenantPropertyRel.objects.get(tenant=request.data["tenant"])
+        payment.rented_property = lease.rented_property
+        
+        # Retrieve the payment type and attach a 
+        # Payment Type instance to the payment
+        payment_type = PaymentType.objects.get(pk=request.data["payment_type"])
+        payment.payment_type = payment_type
+
+        payment.landlord = landlord 
+        payment.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
 
 class PaymentSerializer(serializers.ModelSerializer):
     """JSON serializer for payments"""
