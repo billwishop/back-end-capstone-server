7d2b2e098a0939c3740066e407727a22d6ba548e
billwishop <willbishop.hello@gmail.com>
2021-03-11 13:27:15 -0600

    Retrieve, list, put and delete are set up for the properties resource.

--- crosscheckapi/views/property.py
+++ crosscheckapi/views/property.py
@@ -34,6 +34,69 @@ class Properties(ViewSet):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single property
+        Returns:
+            Response -- JSON serialized property instance
+        """
+        try:
+            rental = Property.objects.get(pk=pk)
+            serializer = PropertySerializer(
+                rental, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """Handle GET requests to property resource
+        Returns:
+            Response -- JSON serialized list of properties
+        """
+        properties = Property.objects.all()
+        landlord = Landlord.objects.get(user=request.auth.user)
+        current_users_properties = Property.objects.filter(landlord=landlord)
+
+        serializer = PropertySerializer(
+            current_users_properties, many=True, context={'request': request})
+
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for properties
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        # landlord = authenticated user
+        landlord = Landlord.objects.get(user=request.auth.user)
+
+        rental = Property.objects.get(pk=pk)
+        rental.street = request.data["street"]
+        rental.city = request.data["city"]
+        rental.state = request.data["state"]
+        rental.postal_code = request.data["postal_code"]
+        rental.landlord = landlord
+
+        rental.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for properties
+        Returns:
+            Response -- 204, 404, or 500 status code
+        """
+        try:
+            rental = Property.objects.get(pk=pk)
+            rental.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Property.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 class PropertySerializer(serializers.ModelSerializer):
     """JSON serializer for properties"""
     class Meta:
